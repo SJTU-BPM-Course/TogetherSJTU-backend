300f5507ac8a3cb904d119c08e0d1c4d6483bde7
sinkinben <sinkinben@outlook.com>
2020-12-06 18:53:47 +0800

    Activity Controller: init Post mapping demo

--- src/main/java/com/sjtu/together/controller/ActivityController.java
+++ src/main/java/com/sjtu/together/controller/ActivityController.java
@@ -1,9 +1,10 @@
 package com.sjtu.together.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.sjtu.together.service.ActivityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RestController
@@ -13,4 +14,12 @@ public class ActivityController {
     ActivityService activityService;
 
     // TODO: 完成活动的增删改查
+
+    @CrossOrigin
+    @PostMapping(value = "/api/activity/add")
+    @ResponseBody
+    public String addActivity(@RequestBody String json, @RequestBody int actid) {
+        System.out.println(json);
+        return JSON.toJSONString(true);
+    }
 }
